0e2364569b23320121abee00f2989d13d355618b
wibinabae <syarifh430@gmail.com>
2025-07-10 21:13:54 +0700

    Add Custom 404 Page

---
+++ app/not-found.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="flex justify-center mb-2 font-bold text-7xl items-center">
+        404
+      </h1>
+      <br />
+      <p className="text-gray-500 text-3xl">Halamanya Gak Ada Braaaaaaaaaay</p>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ app/profile/page.jsx
@@ -0,0 +1,28 @@
+"use client"
+import React from "react";
+import {motion} from "framer-motion"
+import StatCard from "@/components/StatCard";
+import { ShoppingBag } from "lucide-react";
+
+const ProfilePage = () => {
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard name="Total Produk" icon={ShoppingBag} value={"4,321"} />
+          <StatCard name="Total Produk" icon={ShoppingBag} value={"4,321"} />
+          <StatCard name="Total Produk" icon={ShoppingBag} value={"4,321"} />
+          <StatCard name="Total Produk" icon={ShoppingBag} value={"4,321"} />
+        </motion.div>
+
+      </main>
+    </div>
+  );
+};
+
+export default ProfilePage;
